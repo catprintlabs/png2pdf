716e36cf9f543a984d942221740cfbcaf243e469
adamcreekroad <adam@adam-george.com>
2020-06-22 11:14:42 -0400

    Update cmake

--- cmake/modules/FindPNG++.cmake
+++ cmake/modules/FindPNG++.cmake
@@ -1,13 +1,23 @@
-find_path(PNG++_INCLUDE_DIR
-  NAMES png++/png.hpp
-  PATHS
-    "${PNG++_DIR}/include"
-    "${PNG++_DIR}/src"
-    "${PNG++_DIR}"
-    /usr/include
-    /usr/local/include
-)
+if(PNG++_FIND_QUIETLY)
+  set(_FIND_PNG_ARG QUIET)
+endif()
 
-if(PNG++_INCLUDE_DIR)
-  set(PNG++_FOUND TRUE)
-endif(PNG++_INCLUDE_DIR)
+find_package(PNG ${_FIND_PNG_ARG})
+
+if(PNG_FOUND)
+  find_path(PNG++_INCLUDE_DIRS
+    NAMES png++/png.hpp
+    PATHS
+      "${PNG++_DIR}/include"
+      "${PNG++_DIR}/src"
+      "${PNG++_DIR}"
+      /usr/include
+      /usr/local/include
+  )
+
+  if(PNG++_INCLUDE_DIRS)
+    set(PNG++_FOUND TRUE)
+    set(PNG++_INCLUDE_DIRS ${PNG++_INCLUDE_DIRS} ${PNG_INCLUDE_DIRS})
+    set(PNG++_LIBRARIES ${PNG_LIBRARIES})
+  endif(PNG++_INCLUDE_DIRS)
+endif(PNG_FOUND)
